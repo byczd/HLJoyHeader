9bc3610315920dc3517330cb5d39627d91e83e39
pyCzd <byczd@163.com>
2019-02-25 17:59:57 +0800

    firstUpdate

---
+++ HLJoyHeader/TestHeaderViewController.m
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//                   佛祖保佑           永无BUG                     //
+////////////////////////////////////////////////////////////////////
+//  TestHeaderViewController.m
+//  HLJoyHeader
+//
+//  Created by 陈志东 on 2019/2/25.
+//  https://github.com/byczd
+//  https://weibo.com/2303542754/profile?topnav=1&wvr=6&is_all=1
+//  Copyright © 2019年 adong. All rights reserved.
+//
+
+#import "TestHeaderViewController.h"
+
+@interface TestHeaderViewController ()
+
+@end
+
+@implementation TestHeaderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
