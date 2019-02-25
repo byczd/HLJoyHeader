70963d8a623cc5c5fb2267a367a709a6554b6e8a
pyCzd <byczd@163.com>
2019-02-25 17:04:21 +0800

    Initial Commit

---
+++ HLJoyHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLJoyHeader
+//
+//  Created by 陈志东 on 2019/2/25.
+//  Copyright © 2019年 adong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLJoyHeader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HLJoyHeader
+//
+//  Created by 陈志东 on 2019/2/25.
+//  Copyright © 2019年 adong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
